f11566abd78f5f573843d4ca54c4ff2e682542bd
mpredelus3 <mpredelus3@gmail.com>
2024-08-13 14:59:26 -0700

    glow effect fixed

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import horiseonImg from '../assets/images/horiseon.jpg'
-import comingSoon from "../assets/images/comingSoon.jpg"
 
 function ProjectCard({ title, description, image, url }) {
   return (
     <Card className="project-card" style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={image} alt={title} style={{ height: '200px', objectFit: 'cover' }} />  {/* Using the image */}
+      <Card.Img variant="top" src={image} alt={title} style={{ height: '200px', objectFit: 'cover' }} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>
           {description} 
         </Card.Text>
-        <Button variant="primary" href={url} target="_blank" rel="noopener noreferrer"className="project-button">View Project</Button>
+        <Button 
+          variant="primary" 
+          href={url} 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="project-button" /* Class for the button */
+        >
+          View Project
+        </Button>
       </Card.Body>
     </Card>
   );
